b36575850cd82ac015d4373f56431a9a2ca80023
kacsx9 <kacpersieg@wp.pl>
2021-01-20 05:08:18 +0100

    nowe mocki filmów

--- src/app/filmShows-mock.ts
+++ src/app/filmShows-mock.ts
@@ -9,5 +9,17 @@ f = new FilmShow('Die Hard', 'action', new Date('2021-01-19'), '17:20', 70, 1);
 filmShows.push(f);
 f = new FilmShow('Die Hard', 'action', new Date('2021-01-21'), '17:15', 70, 1);
 filmShows.push(f);
+f = new FilmShow('Gwiezdne wojny', 'science-fiction', new Date('2021-01-30'), '20:15', 60, 2);
+filmShows.push(f);
+f = new FilmShow('Gwiezdne wojny', 'science-fiction', new Date('2021-01-30'), '21:15', 60, 1);
+filmShows.push(f);
+f = new FilmShow('Gwiezdne wojny', 'science-fiction', new Date('2021-01-30'), '22:15', 60, 3);
+filmShows.push(f);
+f = new FilmShow('Die Hard', 'action', new Date('2021-01-27'), '17:15', 70, 1);
+filmShows.push(f);
+f = new FilmShow('Die Hard', 'action', new Date('2021-01-27'), '21:15', 70, 1);
+filmShows.push(f);
+f = new FilmShow('Die Hard', 'action', new Date('2021-01-28'), '17:15', 70, 1);
+filmShows.push(f);
 
 export const FilmShows = filmShows;
